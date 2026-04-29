bec281e2a2fabb01a6af37ae432e02f0bf393160
Kiến Huy <huykiento@gmail.com>
2026-04-29 17:51:49 +0700

    Fix file encoding and line endings

---
+++ passbook-management-api/src/main/java/com/uit/passbook_management_api/PassbookManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.uit.passbook_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassbookManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassbookManagementApiApplication.class, args);
+	}
+
+}
