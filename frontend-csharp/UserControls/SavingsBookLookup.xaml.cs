c6575386e577eed9d6f0b6b9a254063342e3c872
harutopia0 <tran67563@gmail.com>
2026-05-25 18:53:24 +0700

    refactor: apply MVVM pattern for SavingsBookLookup

--- frontend-csharp/UserControls/SavingsBookLookup.xaml.cs
+++ frontend-csharp/UserControls/SavingsBookLookup.xaml.cs
@@ -1,84 +1,23 @@
-﻿using System.Collections.ObjectModel;
+﻿using frontend_csharp.ViewModels;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Linq;
 
 namespace frontend_csharp.UserControls
 {
     public partial class SavingsBookLookup : UserControl
     {
-        // 1. Dùng ObservableCollection thay vì List
-        private ObservableCollection<SavingsBookModel> _savingsBooks;
+        private SavingsBookLookupViewModel _viewModel;
 
         public SavingsBookLookup()
         {
             InitializeComponent();
+            _viewModel = new SavingsBookLookupViewModel();
+            this.DataContext = _viewModel;
 
-            _savingsBooks = new ObservableCollection<SavingsBookModel>();
-
-            // 2. Gán ItemsSource MỘT LẦN DUY NHẤT ở đây
-            dgvSavingsBooks.ItemsSource = _savingsBooks;
-            icSavingsBooksGrid.ItemsSource = _savingsBooks;
-
-            this.Loaded += SavingsBookLookup_Loaded;
+            this.Loaded += async (s, e) => await _viewModel.InitializeAsync();
         }
-
-        private async void SavingsBookLookup_Loaded(object sender, RoutedEventArgs e)
-        {
-            // Reset về dạng danh sách (DataGrid - Index 0) mỗi khi tab được mở lại
-            ViewToggleListBox.SelectedIndex = 0;
-
-            // Chỉ load lại data nếu danh sách đang trống để tránh spam gọi API khi chuyển tab
-            if (_savingsBooks.Count == 0)
-            {
-                await LoadDataAsync();
-            }
-        }
-
-        private async Task LoadDataAsync()
-        {
-            // 1. Giả lập gọi API (Chạy ngầm không ảnh hưởng UI)
-            var newDataFromApi = await Task.Run(() =>
-            {
-                var data = new System.Collections.Generic.List<SavingsBookModel>();
-                for (int i = 0; i < 15; i++)
-                {
-                    data.Add(new SavingsBookModel
-                    {
-                        Id = $"FIG-12{i}",
-                        SavingsType = "3 tháng",
-                        CustomerName = "Nguyễn Văn A",
-                        Balance = "500.000 VNĐ",
-                        MaturityDate = "05/12/2026",
-                        InterestRate = "10%",
-                        Status = "Hoạt động"
-                    });
-                }
-                return data;
-            });
-
-            _savingsBooks.Clear();
-
-            // 2. BƠM DỮ LIỆU VÀO UI THREAD Ở CHẾ ĐỘ NỀN (BACKGROUND)
-            foreach (var item in newDataFromApi)
-            {
-                await Application.Current.Dispatcher.InvokeAsync(() =>
-                {
-                    _savingsBooks.Add(item);
-                }, System.Windows.Threading.DispatcherPriority.Background);
-            }
-        }
-    }
-
-    public class SavingsBookModel
-    {
-        public string Id { get; set; }
-        public string SavingsType { get; set; }
-        public string CustomerName { get; set; }
-        public string Balance { get; set; }
-        public string MaturityDate { get; set; }
-        public string InterestRate { get; set; }
-        public string Status { get; set; }
     }
 }
