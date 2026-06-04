8d4959dbd8b8a130e08fd13db59ceb8f07d94aa7
estellra <Charafrisksans024@gmail.com>
2026-06-04 20:06:10 +0700

    Remove Username and Password from NhanVienRequest
    Removed Username and Password properties from NhanVienRequest model.

--- frontend-csharp/Models/NhanVienModel/NhanVienRequest.cs
+++ frontend-csharp/Models/NhanVienModel/NhanVienRequest.cs
@@ -16,12 +16,6 @@ namespace frontend_csharp.Models.NhanVienModel
         [JsonPropertyName("cccd")]
         public string Cccd { get; set; }
 
-        [JsonPropertyName("username")]
-        public string Username { get; set; }
-
-        [JsonPropertyName("password")]
-        public string Password { get; set; }
-
         [JsonPropertyName("role")]
         public string Role { get; set; } // "ADMIN" hoặc "NHAN_VIEN"
     }
