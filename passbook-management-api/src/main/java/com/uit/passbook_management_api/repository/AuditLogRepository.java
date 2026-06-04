0e83bc30e5572113f24f782b8a270dfe63fffbd6
estellra <Charafrisksans024@gmail.com>
2026-06-04 10:10:31 +0700

    Them model api cho audit log va nhac nho ngay dao han cua so tiet kiem

--- passbook-management-api/src/main/java/com/uit/passbook_management_api/repository/AuditLogRepository.java
+++ passbook-management-api/src/main/java/com/uit/passbook_management_api/repository/AuditLogRepository.java
@@ -4,6 +4,9 @@ import com.uit.passbook_management_api.entity.AuditLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
+    List<AuditLog> findAllByOrderByThoiGianDesc();
 }
