aa9ccd032ab1b2c2a42101e7dce652f78bb7a204
estellra <Charafrisksans024@gmail.com>
2026-06-04 20:04:54 +0700

    Update lan 2 cai service them nhan vien

---
+++ frontend-csharp/Models/NhanVienModel/CapNhatNhanVienRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace frontend_csharp.Models.NhanVienModel
+{
+    public class CapNhatNhanVienRequest
+    {
+        [JsonPropertyName("hoTen")]
+        public string HoTen { get; set; }
+
+        [JsonPropertyName("soDienThoai")]
+        public string SoDienThoai { get; set; }
+
+        [JsonPropertyName("cccd")]
+        public string Cccd { get; set; }
+    }
+}
